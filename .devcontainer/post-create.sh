060fd8d9eebf160df12c4097038f1ed1ca84366b
Loki <donaldpakkies@gmail.com>
2020-07-25 20:51:21 +0200

    feat: install dependencies and add aliases

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,7 @@
-sudo apt install -y unzip
-sudo apt install -y php-zip
+sudo apt install -y unzip php-zip nano
 
 composer global require laravel/installer
 export PATH="~/.composer/vendor/bin:$PATH"
+
+wget https://gist.githubusercontent.com/donaldp/65c613d6b1c279da07dbfb333cbe7ed9/raw/6be77b27b949c8cbd40e64ea2c964fe492f1b2ec/bash_aliases -O ~/.bash_aliases
 source ~/.bashrc
