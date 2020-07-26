32778921af5cc86f580af3d8855b524b6dd6f02a
Loki <donaldpakkies@gmail.com>
2020-07-26 15:01:44 +0200

    feat: pull aliases from current branch
    And remove install files

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,5 +9,7 @@ sudo apt install -y mysql-server
 composer global require laravel/installer
 export PATH="~/.composer/vendor/bin:$PATH"
 
-wget https://gist.githubusercontent.com/donaldp/65c613d6b1c279da07dbfb333cbe7ed9/raw/6be77b27b949c8cbd40e64ea2c964fe492f1b2ec/bash_aliases -O ~/.bash_aliases
+cp ~/workspace/laravel-codespace/.devcontainer/aliases ~/.bash_aliases
 source ~/.bashrc
+
+rm -rf ~/workspace/laravel-codespace/{.devcontainer,.git,LICENSE,README.md}
